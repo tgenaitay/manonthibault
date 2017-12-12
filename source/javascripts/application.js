d6826abbe9e8164afd77339b75d8e0470f9dbbe2
thibault genaitay <tgenaitay@gmail.com>
2017-12-12 17:14:34 +0800

    navigation and animations are functional

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,3 +1,15 @@
-//= require jquery
-//= require bootstrap
 //= require_tree .
+
+document.addEventListener( 'DOMContentLoaded', function () {
+
+inView('[data-animate-in]').on('enter', function(el){
+        el.classList.add('in-view')});
+inView('[data-animate-in]').on('exit', function(el){
+        el.classList.remove('in-view')});
+
+}, false );
+
+window.addEventListener('scroll', function(e) {
+
+
+});
